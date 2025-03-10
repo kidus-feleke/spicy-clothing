3b008c4dcddcd8f304b434326bac0991ceb0f01f
kidus-feleke <kidusfeleke5@gmail.com>
2025-03-10 12:54:49 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './categories.styles.scss'
+import Directory from './components/Directory/directory.component'
+
+const App=()=>{
+ 
+  
+  return (
+   <Directory />
+  )
+}
+export default App
